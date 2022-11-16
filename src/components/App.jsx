9459fa9e600aa4bde373caa3b4b43aa9792175f5
itsokaytobesergey <by.imbooo@gmail.com>
2022-11-17 02:06:55 +0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import "../App.css"
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/React">
       <div className="App">
         <Routes>
           <Route path="/" element={<MainLayout />}>
